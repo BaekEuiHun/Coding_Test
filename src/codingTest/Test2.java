2c5598513b6f0948aebd996ed977acab2d4bac45
BaekEuiHun <euihunbaek@gmail.com>
2024-06-07 14:00:25 +0900

    init

--- src/codingTest/Test2.java
+++ src/codingTest/Test2.java
@@ -2,7 +2,6 @@ package codingTest;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println(barkingDogProblem(true, 20));
 //        여러분은 아주 크게 짖는 개를 키우고 있습니다.
 //                hour 매개변수는 현재 시각을 의미하고 0~23까지 숫자를 넣을 수 있습니다.
 //                만약 개가 7시 이전이나 20시 이후에 짖으면 매우 곤란해질겁니다.
@@ -11,10 +10,8 @@ public class Test2 {
 //        barkingDogProblem(true, 6) ==> "짖으면 안돼!!"
 //        barkingDogProblem(true, 7) ==> "든든하군!"
 //        barkingDogProblem(false, 5) ==> "든든하군!"
-
-
+        System.out.println(barkingDogProblem(true, 20));
     }
-
     static String barkingDogProblem(boolean barking, int hour) {
         if (barking && (hour < 7 || hour > 20)) {
             return "짖으면 안돼!!";
