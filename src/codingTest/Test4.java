c4b23dee9493330ba80e750289d583d7f317e56b
BaekEuiHun <euihunbaek@gmail.com>
2024-06-10 20:07:31 +0900

    Update Test4.java

--- src/codingTest/Test4.java
+++ src/codingTest/Test4.java
@@ -9,7 +9,7 @@ public class Test4 {
 // 그리고 거꾸로 가장 마지막 문자부터 출력하도록 만들어 보세요.
 
     public static void main(String[] args) {
-        String someTxt = "물방울이 떨어지는 소리를 들으며 나는 한적한 숲속 오두막에서 책을 읽고 있었다.";
+        String someTxt = "물방울이 떨어지는 소리를 들으며 나는한적한 숲속 오두막에서 책을 읽고 있었다.";
         for (int i = 0; i < someTxt.length(); i++) {
             System.out.println(someTxt.charAt(i));
         }
