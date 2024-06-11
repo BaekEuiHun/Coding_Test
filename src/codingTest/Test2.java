c7cc01dd3179a25dca9262040bb8990ddccdde3b
BaekEuiHun <euihunbaek@gmail.com>
2024-06-11 21:59:29 +0900

    Update Test2.java

--- src/codingTest/Test2.java
+++ src/codingTest/Test2.java
@@ -7,7 +7,7 @@ public class Test2 {
 //                만약 개가 7시 이전이나 20시 이후에 짖으면 매우 곤란해질겁니다.
 //                만약 곤란한 상황이 발생하면 "짖으면 안돼!!" 를 반환하세요, 아니라면 "든든하군!" 을 반환하세요.
 //
-//        barkingDogProblem(true, 6) ==> "짖으면 안돼!!"
+//        barkingDogProblem(true, 6) ==> "짖으면안돼!!"
 //        barkingDogProblem(true, 7) ==> "든든하군!"
 //        barkingDogProblem(false, 5) ==> "든든하군!"
         System.out.println(barkingDogProblem(true, 20));
