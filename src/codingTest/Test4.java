e118ea40b87b111ce219c896669d509187aedcd5
BaekEuiHun <euihunbaek@gmail.com>
2024-06-29 21:15:36 +0900

    Update Test4.java

--- src/codingTest/Test4.java
+++ src/codingTest/Test4.java
@@ -11,7 +11,7 @@ public class Test4 {
     public static void main(String[] args) {
         String someTxt = "물방울이 떨어지는 소리를 들으며 나는 한적한 숲속 오두막에서 책을 읽고 있었다.";
         for (int i = 0; i < someTxt.length(); i++) {
-            System.out.println(someTxt.charAt(i));
+            System.out. println(someTxt.charAt(i));
         }
     }
 }
